8a3676965ca301aef9f785f7380eac02eaa72660
MAGAQ <panamasado41@gmail.com>
2026-06-23 21:04:42 -0400

    Añadiendo cosas extras, no esta validando la seguridad, esta creando propias llaves en vez de leer la normal.

--- src/main/java/dsy/bibliotecaam/api_gateway/ApiGatewayApplication.java
+++ src/main/java/dsy/bibliotecaam/api_gateway/ApiGatewayApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.
+				servlet.UserDetailsServiceAutoConfiguration.class
+})
 @EnableDiscoveryClient
 
 /* ACTIVALO SOLO DESPUES QUE HAYAS ACTIVADO EL CONFIG-SERVER
